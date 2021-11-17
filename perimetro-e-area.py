538d989fc1856c8d471d33c812976346eed841e7
thiagolinsazevedo <94180903+thiagolinsazevedo@users.noreply.github.com>
2021-11-17 15:32:25 -0300

    Delete perimetro-e-area.py

--- perimetro-e-area.py
+++
@@ -1,6 +0,0 @@
-medida_do_lado = input('Digite o valor correspondente ao lado de um quadrado: ')
-
-perimetro = int(medida_do_lado) * 4
-area = int(medida_do_lado) ** 2
-
-print('perímetro:' ,perimetro, '-', 'área:', area)
